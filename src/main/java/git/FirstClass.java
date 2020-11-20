c9740da24c6af58f6b46c6545bab4625fac42a21
iosif ameziane <iosif.ameziane@gmail.com>
2020-11-21 00:02:41 +0200

    add secondClass and creat reference in FirstClass for calling seyHello method

--- src/main/java/git/FirstClass.java
+++ src/main/java/git/FirstClass.java
@@ -3,5 +3,8 @@ package git;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("The main method is created");
+        SecondClass secondClass = new SecondClass();
+        secondClass.sayHello();
+
     }
 }
